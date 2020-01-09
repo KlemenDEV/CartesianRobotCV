6ae4c830740916015c1966fe16a144d3249c6dce
Klemen <klemen.pylo@gmail.com>
2020-01-09 11:06:06 +0100

    First working UI for control

--- src/main/java/org/beetron/Main.java
+++ src/main/java/org/beetron/Main.java
@@ -1,11 +1,49 @@
 package org.beetron;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main extends JFrame {
 
-	private Main() {
+	private JTextArea log = new JTextArea();
 
+	private Main(SerialTask serialTask, OpenCVTask openCVTask) {
+		JToolBar controls = new JToolBar();
+		add("North", controls);
+
+		JButton sendData = new JButton("Send points");
+		controls.add(sendData);
+		sendData.addActionListener(e -> {
+			serialTask.sendPoints(
+					new Point(7, 0),
+					new Point(1, 0),
+					new Point(5, 0)
+			);
+		});
+
+		JButton start = new JButton("Start");
+		controls.add(start);
+		start.addActionListener(e -> serialTask.sendData(new byte[] { 0x03 }));
+		start.setBackground(new Color(0, 255, 0));
+
+		JButton stop = new JButton("Stop");
+		controls.add(stop);
+		stop.addActionListener(e -> serialTask.sendData(new byte[] { 0x04 }));
+		stop.setBackground(new Color(255, 234, 0));
+
+		JButton emergency = new JButton("Emergency");
+		controls.add(emergency);
+		emergency.addActionListener(e -> serialTask.sendData(new byte[] { 0x0F }));
+		emergency.setBackground(new Color(255, 0, 0));
+
+		JScrollPane sp = new JScrollPane(log);
+		sp.setPreferredSize(new Dimension(0, 200));
+
+		add("South", sp);
+	}
+
+	void printToLog(String text) {
+		log.append(text);
 	}
 
 	public static void main(String[] args) {
@@ -15,14 +53,18 @@ public class Main extends JFrame {
 		OpenCVTask openCVTask = new OpenCVTask();
 		SerialTask serialTask = new SerialTask();
 
-		new Thread(openCVTask).start();
-		new Thread(serialTask).start();
-
 		SwingUtilities.invokeLater(() -> {
-			Main main = new Main();
+			Main main = new Main(serialTask, openCVTask);
+
+			serialTask.setMain(main);
+
+			main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			main.setSize(900, 550);
 			main.setLocationRelativeTo(null);
 			main.setVisible(true);
+
+			//new Thread(openCVTask).start();
+			new Thread(serialTask).start();
 		});
 	}
 
--- src/main/java/org/beetron/SerialTask.java
+++ src/main/java/org/beetron/SerialTask.java
@@ -4,28 +4,51 @@ import com.fazecast.jSerialComm.SerialPort;
 import com.fazecast.jSerialComm.SerialPortDataListener;
 import com.fazecast.jSerialComm.SerialPortEvent;
 
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.util.List;
+
 public class SerialTask implements Runnable {
+
+	private Main main;
 
 	private SerialPort comPort;
 
-	public SerialPort getSerialPort() {
-		return this.comPort;
+	void setMain(Main main) {
+		this.main = main;
+	}
+
+	void sendPoints(Point... points) {
+		for (Point point : points) {
+			sendData(new byte[]{0x02});
+			wait(20);
+			sendData(float2bytearr(point.getX()));
+			wait(30);
+			sendData(float2bytearr(point.getY()));
+		}
+	}
+
+	void sendData(byte[] data) {
+		comPort.writeBytes(data, data.length);
 	}
 
 	@Override public void run() {
 		for (SerialPort port : SerialPort.getCommPorts()) {
-			if (port.getDescriptivePortName().contains("USB Serial Port")) {
-				System.err.println(port.getDescriptivePortName());
+			if (port.getDescriptivePortName().contains("STLink Virtual COM Port")) {
 				this.comPort = port;
 			}
 		}
 
-		System.out.println("Opening serial port");
+		main.printToLog("Opening serial port\n");
 
 		comPort.openPort();
 
 		comPort.setComPortTimeouts(SerialPort.TIMEOUT_NONBLOCKING, 20, 20);
 		comPort.setComPortParameters(115200, 8, 1, SerialPort.NO_PARITY);
+
+		main.printToLog(comPort.getDescriptivePortName() + " opened\n");
+
+		main.printToLog("Reset board using onboard button\n\n");
 
 		comPort.addDataListener(new SerialPortDataListener() {
 			@Override public int getListeningEvents() {
@@ -38,10 +61,28 @@ public class SerialTask implements Runnable {
 
 				byte[] newData = new byte[comPort.bytesAvailable()];
 				comPort.readBytes(newData, newData.length);
-				System.out.println(new String(newData));
+
+				main.printToLog(new String(newData));
 			}
 		});
-
-		//this.comPort.writeBytes(data, data.length);
 	}
+
+	private void wait(int ms) {
+		try {
+			Thread.sleep(ms);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private byte[] float2bytearr(float num) {
+		int bits = Float.floatToIntBits(num);
+		byte[] bytes = new byte[4];
+		bytes[0] = (byte)(bits & 0xff);
+		bytes[1] = (byte)((bits >> 8) & 0xff);
+		bytes[2] = (byte)((bits >> 16) & 0xff);
+		bytes[3] = (byte)((bits >> 24) & 0xff);
+		return bytes;
+	}
+
 }
